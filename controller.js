f95688148a667378724046b5db440189f00d3f7b
wkwyatt <wkwyatt1@gmail.com>
2015-11-10 10:33:03 -0500

    angular ng-repeat practice

--- controller.js
+++ controller.js
@@ -1,4 +1,41 @@
 var myApp = angular.module('myApp', []);
+myApp.controller("session2Controller", function($scope) {
+	$scope.items = [
+	{
+		name: "Freddy",
+		desc: "Made a football app"
+	},
+	{
+		name: "Chance",
+		desc: "Still Loves Bootstrap"
+	},
+	{
+		name: "Stuart",
+		desc: "Drove a long way to get here today"
+	},
+	{
+		name: "Peter",
+		desc: "Sits in the front row"
+	},
+	{
+		name: "Andrew",
+		desc: "Pro boxer"	
+	},
+	{
+		name: "Will",
+		desc: "The Entrepenuer"
+	},
+	{
+		name: "Oliver",
+		desc: "Eats a lot"
+	},
+	{
+		name: "Rob",
+		desc: "teach .."
+	}];
+
+	// $scope.color = myColor;
+});
 myApp.controller('appController', appController);
 myApp.controller('myController', function($scope) {
 	$scope.classObject = [
